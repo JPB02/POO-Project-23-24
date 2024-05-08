3010b1da2ab986ee221f5ae929155512b12fff0b
pmrobraga <79986282+pmrobraga@users.noreply.github.com>
2024-05-08 16:00:05 +0100

    Add files via upload

---
+++ src/PushUps.java
@@ -0,0 +1,52 @@
+import java.time.LocalDate;
+
+public class PushUps extends Activity {
+    private int repetitions;
+    private int sets;
+
+    public PushUps() {
+        super();
+        this.repetitions = 0;
+        this.sets = 0;
+    }
+
+    public PushUps(String activityID, String description, LocalDate date, int duration, int repetitions, int sets) {
+        super(activityID, description, date, duration);
+        this.repetitions = repetitions;
+        this.sets = sets;
+    }
+
+    public PushUps(PushUps other) {
+        super(other);
+        this.repetitions = other.getRepetitions();
+        this.sets = other.getSets();
+    }
+
+    public int getRepetitions() {
+        return repetitions;
+    }
+
+    public void setRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    @Override
+    public double calories() {
+
+        return 0;
+    }
+
+    @Override
+    public Activity clone() {
+        return new PushUps(this);
+    }
+}
+
