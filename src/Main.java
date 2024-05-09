55d39bcf163a79d3e2cffc6ca06a8806ed7b2f81
João Barbosa <93934411+JPB02@users.noreply.github.com>
2024-05-09 19:32:22 +0100

    added testing for workoutplans

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,8 @@ public class Main {
 
         // Creating an activity
         Running runningActivity = new Running("1", "Running", LocalDate.now(), 30, 5.0);
+        MountainBike mountainBikeActivity = new MountainBike("2", "Mountain Bike", LocalDate.now(), 40, 16, 500);
+        Squat squatActivity = new Squat("3", "Squat", LocalDate.now(), 10, 20, 3);
 
         // Adding the activity to the user
         appManager.addActivity(user, runningActivity);
@@ -32,6 +34,22 @@ public class Main {
         System.out.println("User's activities:");
         for (Activity activity : user.getActivitiesList()) {
             System.out.println(activity);
+        }
+
+        // Creating a workout plan
+        WorkoutPlan workoutPlan = new WorkoutPlan();
+        workoutPlan.addActivity(runningActivity);
+        workoutPlan.addActivity(mountainBikeActivity);
+        workoutPlan.addActivity(squatActivity);
+        workoutPlan.setIterations(3); // Set the number of iterations for the workout plan
+
+        // Adding the workout plan to the user
+        appManager.addWorkoutPlan(user, workoutPlan);
+
+        // Displaying the user's workout plans
+        System.out.println("User's workout plans:");
+        for (WorkoutPlan plan : user.getWorkoutPlansList()) {
+            System.out.println(plan);
         }
     }
 }
